5baa2dfac13ac84d42d58853a5be6f60a468f0e2
liushisheng <3379782451@qq.com>
2025-03-28 15:35:16 +0800

    init插件框架

---
+++ include/qRoadMarking.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "ccStdPluginInterface.h"
+
+/// <summary>
+/// qRoadMarking插件与cc的接口
+/// </summary>
+class qRoadMarking : public QObject, public ccStdPluginInterface
+{
+	Q_OBJECT
+	Q_INTERFACES(ccPluginInterface ccStdPluginInterface)
+
+	Q_PLUGIN_METADATA(IID "cccorp.cloudcompare.plugin.qRoadMarking" FILE "../info.json")
+
+public:
+	explicit qRoadMarking(QObject* parent = nullptr);
+	~qRoadMarking() override = default;
+
+	QString getName() const override { return "RoadMarking"; }
+	QString getDescription() const override { return "Extracts road markings from point clouds"; }
+	QIcon getIcon() const override;
+
+	void onNewSelection(const ccHObject::Container& selectedEntities) override;
+
+	QList<QAction*> getActions() override;
+
+
+protected:
+
+
+private slots:
+};
---
+++ src/qRoadMarking.cpp
@@ -0,0 +1,33 @@
+﻿#include "qRoadMarking.h"
+
+qRoadMarking::qRoadMarking(QObject* parent)
+	: QObject(parent),
+	ccStdPluginInterface(":/CC/plugin/qRoadMarking/info.json")
+{
+}
+
+QIcon qRoadMarking::getIcon() const
+{
+	return QIcon("");
+}
+
+void qRoadMarking::onNewSelection(const ccHObject::Container& selectedEntities)
+{
+	// 判断是否选中了点云
+	bool hasCloud = false;
+	for (ccHObject* entity : selectedEntities)
+	{
+		if (entity && entity->isA(CC_TYPES::POINT_CLOUD))
+		{
+			hasCloud = true;
+			break;
+		}
+	}
+
+	// 如果选择了点云
+}
+
+QList<QAction*> qRoadMarking::getActions()
+{
+	return QList<QAction*>{ nullptr };
+}
