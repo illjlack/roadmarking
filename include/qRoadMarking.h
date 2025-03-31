1ac9e8cf40c823d38064eca8ba46c9c884200af1
liushisheng <3379782451@qq.com>
2025-03-31 10:42:36 +0800

    简单实现插件窗口

--- include/qRoadMarking.h
+++ include/qRoadMarking.h
@@ -1,4 +1,5 @@
 #pragma once
+#pragma execution_character_set("utf-8")
 
 #include "ccStdPluginInterface.h"
 
@@ -24,9 +25,13 @@ public:
 
 	QList<QAction*> getActions() override;
 
+	void doAction();
+
 
 protected:
 
+private:
+	QAction* m_startDlg = nullptr;
 
 private slots:
 };
--- src/qRoadMarking.cpp
+++ src/qRoadMarking.cpp
@@ -1,5 +1,13 @@
 ﻿#include "qRoadMarking.h"
 
+#include <QObject>
+#include <QPoint>
+#include <QMouseEvent>
+#include <QCoreApplication>
+
+#include "ccGLWindow.h"
+#include "qSignExtractDlg.h"
+
 qRoadMarking::qRoadMarking(QObject* parent)
 	: QObject(parent),
 	ccStdPluginInterface(":/CC/plugin/qRoadMarking/info.json")
@@ -24,10 +32,57 @@ void qRoadMarking::onNewSelection(const ccHObject::Container& selectedEntities)
 		}
 	}
 
-	// 如果选择了点云
+	if (m_startDlg)
+	{
+		m_startDlg->setEnabled(hasCloud);
+	}
 }
 
 QList<QAction*> qRoadMarking::getActions()
 {
-	return QList<QAction*>{ nullptr };
+	if (!m_startDlg)
+	{
+		m_startDlg = new QAction(getName(), this);
+		m_startDlg->setToolTip(getDescription());
+		m_startDlg->setIcon(getIcon());
+		connect(m_startDlg, &QAction::triggered, this, &qRoadMarking::doAction);
+	}
+	return QList<QAction*>{ m_startDlg };
+}
+
+void qRoadMarking::doAction()
+{
+	if (!m_app)
+	{
+		assert(false);
+		return;
+	}
+
+
+	const ccHObject::Container& selectedEntities = m_app->getSelectedEntities();
+
+	if (!m_app->haveOneSelection() || !selectedEntities.front()->isA(CC_TYPES::POINT_CLOUD))
+	{
+		m_app->dispToConsole("Select one cloud!", ccMainAppInterface::ERR_CONSOLE_MESSAGE);
+		return;
+	}
+
+	ccHObject* cloud = selectedEntities.front();
+	
+	qSignExtractDlg dlg(m_app);
+
+	//the widget should be visible before we add the cloud
+	dlg.show();
+	QCoreApplication::processEvents();
+
+	//automatically deselect the input cloud
+	m_app->setSelectedInDB(cloud, false);
+
+	if (dlg.setCloud((ccPointCloud*)cloud))
+	{
+		dlg.exec();
+	}
+
+	//currently selected entities appearance may have changed!
+	m_app->refreshAll();
 }
